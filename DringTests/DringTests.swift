9095eaf629ff26f5265d3c36d413125a8800943e
刘兴明 <liuxingming@liuxingmings-MacBook-Pro.local>
2017-01-16 22:35:53 +0800

    Initial Commit

---
+++ DringTests/DringTests.swift
@@ -0,0 +1,36 @@
+//
+//  DringTests.swift
+//  DringTests
+//
+//  Created by 刘兴明 on 16/01/2017.
+//  Copyright © 2017 刘兴明. All rights reserved.
+//
+
+import XCTest
+@testable import Dring
+
+class DringTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
