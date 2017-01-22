164e32b333da2ec821d1bea7873bdc45963b3dde
刘兴明 <liuxingming@liuxingmings-MacBook-Pro.local>
2017-01-22 22:21:46 +0800

    Add veri coordinate

--- Dring/ViewController.swift
+++ Dring/ViewController.swift
@@ -10,20 +10,29 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    func drawCoord()->Void{
+    func drawVCoord(rect:CGRect)->Void{
         let layer=CAShapeLayer()
-        layer.frame=CGRect(x:0, y:50, width:150, height:100)
+        layer.frame=CGRect(x:0, y:50, width:rect.width, height:rect.height-50-50)
         //layer.backgroundColor=UIColor.black.cgColor
         //view.layer.addSublayer(layer)
         
         //利用UIBezierPath绘制简单的矩形
         let path=UIBezierPath()
+        path.move(to: CGPoint(x:0,y:0))
+        path.addLine(to: CGPoint(x:layer.frame.width,y:0))
+        path.addLine(to: CGPoint(x:layer.frame.width,y:layer.frame.height))
+        path.addLine(to: CGPoint(x:0,y:layer.frame.height))
+        path.addLine(to: CGPoint(x:0,y:0))
+        
         path.move(to:CGPoint(x:40,y:0))
-        path.addLine(to:CGPoint(x:40,y:300))
+        path.addLine(to:CGPoint(x:40,y:layer.frame.height))
+        path.move(to:CGPoint(x:30,y:layer.frame.height))
+        path.addLine(to:CGPoint(x:40,y:layer.frame.height))
+        
         //let layer=CAShapeLayer()
         layer.path=path.cgPath
         //填充颜色
-        layer.fillColor=UIColor.black.cgColor
+        layer.fillColor=UIColor.clear.cgColor
         //边框颜色
         layer.strokeColor=UIColor.black.cgColor
         view.layer.addSublayer(layer)
@@ -32,7 +41,11 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        drawCoord()
+        //获取屏幕大小（不包括状态栏高度）
+        let viewBounds = CGRect(x:0,y:20,
+                                       width:UIScreen.main.bounds.width,
+                                       height:UIScreen.main.bounds.height-20)
+        drawVCoord(rect: viewBounds)
     }
 
     override func didReceiveMemoryWarning() {
