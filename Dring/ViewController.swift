4442acede557ae116f18264cec00599912e637bd
刘兴明 <liuxingming@liuxingmings-MacBook-Pro.local>
2017-03-12 22:10:09 +0800

    Initial Commit

---
+++ Dring/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Dring
+//
+//  Created by 刘兴明 on 12/03/2017.
+//  Copyright © 2017 刘兴明. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
